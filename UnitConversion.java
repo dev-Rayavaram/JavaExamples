43a27508a8d4e29adb3dea4f3a80fc70458dd547
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-29 15:22:46 -0500

    updated Unit conversion to calculate number of  meteres when inches is given in integer

--- UnitConversion.java
+++ UnitConversion.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class UnitConversion {
     public static void main (String[] args){
-        System.out.println("Enter first input of float type");
+        System.out.println("Enter number of inches in integer type");
         Scanner in = new Scanner(System.in);
-        float var1 = in.nextFloat();
-        System.out.println("Enter second input of integer type");
-        int var2 =  in.nextInt();
-        float result =var1*var2;
-        System.out.print(result);
+        int var1 = in.nextInt();
+        //number of meters for inch 0.0254
+         float result =var1*0.0254;
+        System.out.println("Number of meters per "+var1+" inches is "+result);
 
     }
 }
